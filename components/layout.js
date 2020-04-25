ddce387f3bf753b6c2792fc413e645474e5097fc
Romina Villaverde <romina.villaverde@gmail.com>
2020-04-25 18:45:53 -0500

    first commit

---
+++ components/layout.js
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+import styles from './layout.module.scss'
+import utilStyles from '../styles/utils.module.scss'
+
+const name = 'I want pizza'
+export const siteTitle = 'I want pizza'
+
+export default function Layout({ children, home }) {
+  return (
+    <div>
+      <Head>
+        <title>I want pizza!</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900|Rammetto+One&display=swap" rel="stylesheet" />
+        <meta
+          name="I want pizza"
+          content="Order your pizza from the best place in town!"
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        <Link href="/">
+          <a>
+            <img
+              src="/images/logo.svg"
+              className={`${styles.headerImage}`}
+              alt={name}
+            />
+          </a>
+        </Link>
+      </header>
+
+      <main className={styles.container}>{children}</main>
+
+      <footer>
+        <p>©2020 - I WANT PIZZA</p>
+      </footer>
+
+      <style jsx global>{`
+        html,
+        body {
+          margin: 0;
+        }
+      `}</style>
+    </div>
+  )
+}
